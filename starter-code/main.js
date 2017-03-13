7fa4a2d4824191acf91975038a1b8b9aa63bfb15
erin dawson <erindawson@erins-MacBook-Pro.local>
2017-03-12 22:54:01 -0400

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,11 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardFour === cardThree) {
+	alert ("You found a match!");
+} else if (cardOne === cardTwo) {
+	alert ("You found a match!");
+} else {
+	alert ("Sorry, try again.");
+}
